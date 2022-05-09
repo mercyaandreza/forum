69ce9d675b02d07bf96d6ca667c81de6bdd5b9e3
Andreza Mércya <deza_mercya@hotmail.com>
2022-05-09 12:57:14 -0300

    initial

---
+++ src/main/java/br/com/alura/forum/controller/helloController.java
@@ -0,0 +1,18 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class helloController {
+
+    @Controller
+    public static class HelloController {
+
+        @RequestMapping("/")
+        @ResponseBody
+        public String hello(){
+            return "hello word aula 1!";
+        }
+    }
+}
